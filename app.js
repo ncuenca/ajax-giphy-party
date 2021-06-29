7078fdcc5273d8f4179de0534aa1c54d148f4e94
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-06-29 16:01:36 -0700

    finished

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Let's get this party started!");
+"use strict"
+
+const BASE_URL;
+const API_KEY;
+const GIF_AREA = $("#gifs");
+
+/** getGif: finds gif and adds it to DOM */
+async function getGif(evt) {
+    evt.preventDefault();
+    let search = $("#text-input").val();
+    // let api_key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    // let url = "http://api.giphy.com/v1/gifs/search"
+    let url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`;
+    let response = await axios.get(url);
+
+    let gifURL = response.data.data[0].images.original.url;
+    let gif = $(`<img src = ${gifURL} alt = ${title}">`);
+    GIF_AREA.append(gif);
+}
+
+/** removeGifs: removes gifs */
+function removeGifs(evt) {
+    evt.preventDefault();
+    GIF_AREA.empty();
+}
+
+$("#submit").click(getGif);
+$("#remove").click(removeGifs);
